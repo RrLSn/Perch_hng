344c9034c1c2b163b3aa73cb819cef92d751ee6c
RrLSn <afolabisodiqtemitope@yahoo.com>
2024-07-08 19:35:53 +0100

    updates

--- src/Pages/Checkout.jsx
+++ src/Pages/Checkout.jsx
@@ -8,16 +8,48 @@ const Checkout = () => {
   return (
     <div className={styles.container}>
       <NavBar />
-      <main className={styles.checkOut}>
+      <div className={styles.checkOut}>
         <h1>Checkout</h1>
         <div className={styles.content}>
           <div className={styles.buyerInfo}>
-            <div className={styles.address}></div>
-            <div className={styles.payInfo}></div>
+            <div className={styles.address}>
+              <hp>Shipping address</hp>
+              <form className={styles.addForm}>
+                <div>
+                  <p></p>
+                  <span>
+                    <input type="text" />
+                    <img src="" alt="icon" />
+                  </span>
+                </div>
+                {/* <div>
+                  <p></p>
+                  <span>
+                    <input type="text" />
+                    <img src="" alt="icon" />
+                  </span>
+                </div>
+                <div>
+                  <p></p>
+                  <span>
+                    <input type="text" />
+                    <img src="" alt="icon" />
+                  </span>
+                </div>
+                <div>
+                  <p></p>
+                  <span>
+                    <input type="text" />
+                    <img src="" alt="icon" />
+                  </span>
+                </div> */}
+              </form>
+            </div>
+            {/* <div className={styles.payInfo}></div> */}
           </div>
-          <div></div>
+          {/* <div></div> */}
         </div>
-      </main>
+      </div>
       <Footer />
     </div>
   );
